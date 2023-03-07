7816385e045928eaa4954e321b8bfd7a99d2f8cb
Brunno Vanelli <brunnovanelli@gmail.com>
2023-03-07 21:15:32 +0100

    feat: Finish the script for parsing a single source.

--- index.js
+++ index.js
@@ -1,12 +1,62 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
+import moment from "moment";
 
-const axiosResponse = await axios.request({
-  method: "GET",
-  url: "https://mcarena-esslingen.de/reservations.php?action=showReservations&type_id=3&date=2023-03-10",
-})
+class ParsedData {
+  constructor(source_website, court, time_slot_start, time_slot_end, is_available) {
+    this.source_website = source_website;
+    this.court = court;
+    this.time_slot_start = time_slot_start;
+    this.time_slot_end = time_slot_end;
+    this.is_available = is_available;
+  }
+}
 
-const $ = cheerio.load(axiosResponse.data)
+async function scrape_mcarena_esslingen(requested_date) {
+  const endpoint_url = "https://mcarena-esslingen.de/reservations.php";
+  const axiosResponse = await axios.request({
+    method: "GET",
+    params: {
+      action: "showReservations",
+      type_id: "3", // ???
+      date: requested_date.format("YYYY-MM-DD"),
+    },
+    url: endpoint_url,
+  })
 
-const someThing = $('table[class=day-reservations-table-area]')
-console.log(someThing.html())
+  const $ = cheerio.load(axiosResponse.data);
+
+  let availabilities = [];
+
+  const tables = $('table[class=day-reservations-table-area]')
+  tables.each(function (i, elm) {
+    const location = $(this).find("tr").find("th").first().text().trim();
+    const result = $(this).find("tr").map(function (i_inner, elm_inner) {
+      const time_slot_str = $(elm_inner).find("td").text().trim();
+      if (time_slot_str === "") {
+        return
+      }
+      // check if available
+      const is_available = ($(elm_inner).find("td").first().attr("class") == 'available');
+      // now build the object
+      // split and re-parse the date in the format '08:00 - 08:30'
+      const time_slots = time_slot_str.split(" - ", 2).map((value) => moment(`${requested_date.format("YYYY-MM-DD")} ${value}`, "YYYY-MM-DD hh:mm"));
+      availabilities.push(new ParsedData(
+        endpoint_url, location, time_slots[0], time_slots[1], is_available
+      ))
+    })
+  })
+  // sort
+  availabilities.sort((a, b) => (a.time_slot_start - b.time_slot_start));
+  return availabilities;
+}
+
+
+const requested_date = new moment("2023-03-10");
+const availabilities = await scrape_mcarena_esslingen(requested_date);
+// now for the parser we print only available slots
+const date_format = "DD MMM hh:mm"
+availabilities.forEach((elem) => {
+  if (elem.is_available)
+    console.log(`Court ${elem.court} is available between ${elem.time_slot_start.format(date_format)} and ${elem.time_slot_end.format(date_format)}`)
+});
