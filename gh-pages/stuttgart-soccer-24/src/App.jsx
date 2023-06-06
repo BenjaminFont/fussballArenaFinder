a93054277e648744caadfcfb263c7bef7cca7dfa
Benny Burkert <benny.burkert@codecentric.de>
2023-06-06 20:22:51 +0200

    Create Table from new data draft

--- gh-pages/stuttgart-soccer-24/src/App.jsx
+++ gh-pages/stuttgart-soccer-24/src/App.jsx
@@ -1,64 +1,69 @@
-import './App.css';
-import data from './scraped/mac-arena-2023-05-10.json';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Unstable_Grid2';
-import moment from 'moment';
+import data from './scraped/mac-arena-2023-05-10-new.json'
 
-function App() {
-
-
-  return (
-    <>
-      <AutoGrid />
-    </>
-  );
+function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
 }
 
-export default App
+// const rows = [
+//     createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+//     createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+//     createData('Eclair', 262, 16.0, 24, 6.0),
+//     createData('Cupcake', 305, 3.7, 67, 4.3),
+//     createData('Gingerbread', 356, 16.0, 49, 3.9),
+// ];
 
-function AutoGrid() {
-  return (
-    <Box>
-      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-        {data.map((e) => {
-          // only create grids for the available slots
-          return e.is_available ? <Grid xs={3}>
-            <BasicCard  {...e} />
-          </Grid> : null;
-        })}
-      </Grid>
-    </Box>
-  );
+const startTime = "08:00:00.000Z"
+const endTime = "22:00:00.000Z"
+let headers = []
+let rows = [["T1",true,true], ["T2",false,false], ["T3",false,true]]
+
+function createTable(){
+    data.forEach((court) => {
+        headers.push(court.court)
+    })
+}
+createTable()
+
+function Cell({label, index}){
+    if(index == 0){
+      return <TableCell component="th">{label}</TableCell>
+    }
+    return <TableCell align="right">{label.toString()}</TableCell>
 }
 
-function BasicCard({
-  source_website,
-  court,
-  time_slot_start,
-  time_slot_end,
-  is_available,
-}) {
-  const displayFormat = "DD MMM HH:mm"
-  return (
-    <Card sx={{ minWidth: 275 }}>
-      <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          {court}
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          {moment(time_slot_start).format(displayFormat)} to {moment(time_slot_end).format(displayFormat)}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small" href={source_website}>Go to website</Button>
-      </CardActions>
-    </Card>
-  );
+export default function BasicTable() {
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Timeslot</TableCell>
+                        {headers.map((name) => (
+                            <TableCell key={name} align="right">{name}</TableCell>
+                        ))}
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows.map((row) => (
+                        <TableRow
+                            key={row[0]}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                            {row.map((cell, index)=>(
+                                <Cell key={index} label={cell} index={index}/>
+                            ))}
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
 }
