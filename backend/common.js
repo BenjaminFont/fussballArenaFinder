234b734585740e51b2b30722d14d9515e9100e38
Brunno Vanelli <brunnovanelli@gmail.com>
2023-07-07 21:33:54 +0200

    feat: Add multi picker to frontend with filter.

--- backend/common.js
+++ backend/common.js
@@ -1,4 +1,7 @@
 import fs from "fs";
+import path from "path";
+import { fileURLToPath } from 'url';
+
 
 export class ArenaEntry {
   constructor(source_website, court, day, results, parsed_at) {
@@ -11,18 +14,23 @@ export class ArenaEntry {
 }
 
 export class TimeSlot {
-  constructor(time_slot_start, time_slot_end) {
+  constructor(time_slot_start, time_slot_end, is_available) {
     this.time_slot_start = time_slot_start;
     this.time_slot_end = time_slot_end;
+    this.is_available = is_available;
   }
 }
 
 
 export function writeToFile(availabilities, requestedDate) {
-  // export to a files
+  // find relative path, in case script is executed from different directory
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const relativePath = path.join(__dirname, '..', `gh-pages/stuttgart-soccer-24/src/scraped/data.json`);
+  // export to file
   const jsonStr = JSON.stringify(availabilities, null, 2);
   fs.writeFileSync(
-    `../gh-pages/stuttgart-soccer-24/src/scraped/data.json`,
+    relativePath,
     jsonStr,
     'utf8'
   );
--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,12 @@ import moment from "moment";
 import { writeToFile } from "./common.js";
 import { scrape } from './parsers/mcarena.js';
 
+// parse from argv
+const arg = process.argv[2];
 
-const parsingDate = "2023-07-05"
+const parsingDate = arg ? moment(arg, 'YYYY-MM-DD') : moment().startOf('day');
 const requestedDate = new moment(parsingDate);
+console.log(`Running scraper at the date ${requestedDate}`);
 const availabilities = await scrape(requestedDate);
 // now for the parser we print only available slots
 // const date_format = "DD MMM hh:mm"
