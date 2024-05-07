e73494b009d4d1557c73fb28d4987ccebdefc662
Christoph Muth <christoph.muth@codecentric.de>
2024-05-07 17:59:02 +0200

    add new parser

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import moment from "moment";
-import { writeToFile } from "./common.js";
-import { scrape } from './parsers/mcarena.js';
-
+import {writeToFile} from "./common.js";
+import {scrape} from './parsers/mcarena.js';
+import {retrieveAvailableSlots} from './parsers/hallofsoccer.js';
 
 // parse from argv
 const arg = process.argv[2];
@@ -9,11 +9,16 @@ const parsingDate = arg ? moment(arg, 'YYYY-MM-DD') : moment().startOf('day');
 const endDate = parsingDate.clone().add(3, 'days');
 // generate filenames
 const fileNames = ["data_today", "data_tomorrow", "data_overmorrow"];
-var i = 0;
-for (var m = parsingDate.clone(); m.isBefore(endDate); m.add(1, 'days')) {
+let i = 0;
+for (let m = parsingDate.clone(); m.isBefore(endDate); m.add(1, 'days')) {
   const requestedDate = new moment(m);
   console.log(`Running scraper at the date ${requestedDate}`);
-  const availabilities = await scrape(requestedDate);
+
+  const getAvailableMcArenaSlots = scrape(requestedDate);
+  const getAvailableHallofsoccerSlots = retrieveAvailableSlots(requestedDate);
+
+  const availabilities = (await Promise.all([getAvailableMcArenaSlots, getAvailableHallofsoccerSlots])).flat(1);
+
   writeToFile(availabilities, fileNames[i]);
   i += 1;
 }
---
+++ backend/parsers/hallofsoccer.js
@@ -0,0 +1,45 @@
+import {ArenaEntry, TimeSlot} from "../common.js";
+import moment from "moment-timezone";
+
+
+function groupBy(array, keySelector) {
+  const grouped = {};
+  for (const item of array) {
+    const key = keySelector(item);
+    if (!grouped[key]) {
+      grouped[key] = [];
+    }
+    grouped[key].push(item);
+  }
+  return grouped;
+}
+
+export async function retrieveAvailableSlots(requested_date) {
+  const court_meta = {
+    name: "Hall of soccer",
+    url: "https://www.eversports.de/widget/w/sao3ov",
+    apiEndpoint: `https://www.eversports.de/widget/api/slot?facilityId=68770&sport=fussball&startDate=${requested_date.format("YYYY-MM-DD")}&courts%5B%5D=53694&courts%5B%5D=53695&courts%5B%5D=53696&courts%5B%5D=53697`
+  };
+
+  const response = await fetch(court_meta.apiEndpoint);
+  const {slots: bookedSlots} = await response.json()
+
+  const bookedSlotsToday = bookedSlots.filter(slot => slot.date === `${requested_date.format('YYYY-MM-DD')}`)
+  const numberOfCourts = Object.keys(groupBy(bookedSlotsToday, slot => slot.court)).length
+  const bookedSlotsTodayByStartTime = groupBy(bookedSlotsToday, slot => slot.start)
+
+  const timesWhenNoCourtIsAvailable = Object.entries(bookedSlotsTodayByStartTime).map(([startTime, slot]) => {
+    return slot.length === numberOfCourts ? parseInt(startTime.substring(0,2)) : undefined
+  }).filter(a => a)
+
+  const timesWhenACourtIsAvailable = []
+  for (let i = 10; i <= 23; i++) {
+    !timesWhenNoCourtIsAvailable.includes(i) && timesWhenACourtIsAvailable.push(i);
+  }
+
+  const availabilities = timesWhenACourtIsAvailable.map(timeWhenACourtIsAvailable => {
+    return new TimeSlot(moment(requested_date).set({hours: timeWhenACourtIsAvailable}),moment(requested_date).set({hours: timeWhenACourtIsAvailable}).add(1, 'hours'), true)
+  })
+
+  return new ArenaEntry(court_meta.url, 'Hall of soccer', requested_date, availabilities, moment())
+}
