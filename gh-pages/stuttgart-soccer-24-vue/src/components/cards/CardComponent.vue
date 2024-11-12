0e31cc82f5db91f120f17085aa822d1c7f0a077e
Brunno Vanelli <bvanelli@users.noreply.github.com>
2024-11-12 19:53:19 +0100

    fix: Fix timezone problem with i18n. (#52)

--- gh-pages/stuttgart-soccer-24-vue/src/components/cards/CardComponent.vue
+++ gh-pages/stuttgart-soccer-24-vue/src/components/cards/CardComponent.vue
@@ -1,30 +1,38 @@
 <template>
-    <div
-            class="card-wrapper col-1 col-md-2 col-md-3 col-lg-4 col-xl-3"
-            v-on:click="redirectToUrl"
-            v-bind:class="{'not-available':!data.availableCard}"
-    >
-        <div class="title">{{data.court}}</div>
-        <div class="pre-title">{{data.datePart}}</div>
-        <div class="description">{{data.dateStart}}</div>
-        <div class="second-description"><img src="./../../assets/icons/clock.svg" width="20" height="20">{{data.duration}}m</div>
+  <div
+      class="card-wrapper col-1 col-md-2 col-md-3 col-lg-4 col-xl-3"
+      v-on:click="redirectToUrl"
+      v-bind:class="{'not-available':!data.availableCard}"
+  >
+    <div class="title">{{ data.court }}</div>
+    <div class="pre-title">{{ data.date ? $d(data.date, 'day') : '' }}</div>
+    <div class="description">{{ data.date ? $d(data.date, 'hour') : '' }}</div>
+    <div class="second-description">
+      <font-awesome-icon :icon="'fa-regular fa-clock'" style="width: 20px; height: 20px; margin-right: 5px"></font-awesome-icon>
+      {{ data.duration }}m
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        props: {
-            data: {
-                type: Object,
-                required: true
-            },
-        },
-        methods: {
-            redirectToUrl: function () {
-                if(this.data.availableCard) {
-                    window.open(this.data.sourceWebsite, '_blank');
-                }
-            }
-        }
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
+
+export default {
+  components: {
+    FontAwesomeIcon
+  },
+  props: {
+    data: {
+      type: Object,
+      required: true
+    },
+  },
+  methods: {
+    redirectToUrl: function () {
+      if (this.data.availableCard) {
+        window.open(this.data.sourceWebsite, '_blank');
+      }
     }
+  }
+}
 </script>
