3d8f0bee78a5c035c4e47242321d5c2fe32af593
Brunno Vanelli <bvanelli@users.noreply.github.com>
2024-11-12 20:48:11 +0100

    refactor: Replace momentjs by dayjs. (#53)

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,35 @@
-import moment from "moment";
+import dayjs from "dayjs";
+import utc from 'dayjs/plugin/utc.js';
+import timezone from 'dayjs/plugin/timezone.js';
 import {writeToFile} from "./common.js";
 import {scrape} from './parsers/mcarena.js';
 import {retrieveAvailableSlots} from './parsers/hallofsoccer.js';
 import soccerOlympScraper from "./parsers/soccerolymp.js";
 
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
 // parse from argv
 const arg = process.argv[2];
-const parsingDate = arg ? moment(arg, 'YYYY-MM-DD') : moment().startOf('day');
-const endDate = parsingDate.clone().add(3, 'days');
+const parsingDate = arg ? dayjs(arg, 'YYYY-MM-DD') : dayjs().startOf('day');
 // generate filenames
-const fileNames = ["data_today", "data_tomorrow", "data_overmorrow"];
-let i = 0;
-for (let m = parsingDate.clone(); m.isBefore(endDate); m.add(1, 'days')) {
-  const requestedDate = new moment(m);
-  console.log(`Running scraper at the date ${requestedDate}`);
+const fileNames = {
+    "data_today": parsingDate,
+    "data_tomorrow": parsingDate.add(1, 'days'),
+    "data_overmorrow": parsingDate.add(2, 'days'),
+};
 
-  const getAvailableMcArenaSlots = scrape(requestedDate);
-  const getAvailableHallofsoccerSlots = retrieveAvailableSlots(requestedDate);
-  const getAvialbleSoccerOlympSlots = soccerOlympScraper(requestedDate);
+for (const [filename, m] of Object.entries(fileNames)) {
+    const requestedDate = new dayjs(m);
+    console.log(`Running scraper at the date ${requestedDate}`);
 
-  const availabilities = (await Promise.all([getAvailableMcArenaSlots, getAvailableHallofsoccerSlots, getAvialbleSoccerOlympSlots])).flat(1);
+    const getAvailableMcArenaSlots = scrape(requestedDate);
+    const getAvailableHallofsoccerSlots = retrieveAvailableSlots(requestedDate);
+    const getAvialbleSoccerOlympSlots = soccerOlympScraper(requestedDate);
 
-  writeToFile(availabilities, fileNames[i]);
-  i += 1;
+    const availabilities = (await Promise.all([
+        getAvailableMcArenaSlots, getAvailableHallofsoccerSlots, getAvialbleSoccerOlympSlots
+    ])).flat(1);
+
+    writeToFile(availabilities, filename);
 }
